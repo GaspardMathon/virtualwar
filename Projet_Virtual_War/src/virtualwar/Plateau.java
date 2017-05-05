ab3fd164c831aeb61642d1a5c7996cdb4bbbfc26
Gaspard MATHON <gaspard.mathon@hotmail.fr>
2017-05-05 14:00:36 +0200

    ajout/supppr des images pour les mettre dans le bon repertoire

--- Projet_Virtual_War/src/virtualwar/Plateau.java
+++ Projet_Virtual_War/src/virtualwar/Plateau.java
@@ -9,7 +9,7 @@ public class Plateau {
 	private int hauteur;
 	private int largeur;
 	private Cellule[][] grille;
-	private String[] images = new String[]{}; 
+	private String[] images = new String[]{"base.jpg","char.jpg","index.jpg","obstacle.jpg","piegeur.jpg","tireur.jpg"}; 
 	
 	//Constructeur prenant deux entiers en paramêtre, place deux bases opposées et des obstacles tout autour de la grille de jeu
 	public Plateau(int hauteur, int largeur){
